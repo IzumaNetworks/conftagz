284c85bf76d3d508d76fe4bd317c66da91774f16
Janne Kiiskilä <janne.kiiskila@izumanetworks.com>
2024-07-10 23:43:55 +0300

    cobratags.go - add missing error catching
    We check for err, but it's not taken from the previous call.
    Its something from way earlier.

--- cobratags.go
+++ cobratags.go
@@ -556,7 +556,7 @@ func ProcessCobraTags(somestruct interface{}, opts *CobraFieldSubstOpts) (ret *P
 						if len(tag) > 0 {
 							existing, ok := ret.needflags[tag] // check if we already have a retriever for this flag
 							if ok {
-								setflagValPtr(parentpath, field.Name, fieldValue, tag, stag, usagetag, existing, pflags)
+								_, err = setflagValPtr(parentpath, field.Name, fieldValue, tag, stag, usagetag, existing, pflags)
 								if err != nil {
 									return
 								}
