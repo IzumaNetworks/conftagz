2e9839c5c3a05b8f87281af33b17255d5268858b
Janne Kiiskilä <janne.kiiskila@izumanetworks.com>
2024-07-11 15:53:31 +0300

    Fix tests - ensure no test configs enter the apps
    Seems we leak some go test stuff into the test, which messes up
    the config. Ensure we start the examples w/o arguments.

--- examples/example/app_test.go
+++ examples/example/app_test.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestExampleAppExecution(t *testing.T) {
+	originalArgs := os.Args
+	// Ensure os.Args is restored after this test
+	defer func() { os.Args = originalArgs }()
+
+	// No arguments
+	os.Args = []string{""}
 	RunMain()
 	t.Log("Example ran successfully")
 }
--- examples/examplecobra/app_test.go
+++ examples/examplecobra/app_test.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 // Build and run the application to test it.
 // Any issues in the example should return non-zero exit code.
 func TestExampleCobraExecution(t *testing.T) {
+	originalArgs := os.Args
+	// Ensure os.Args is restored after this test
+	defer func() { os.Args = originalArgs }()
+	// No arguments - original arguments can contain some test configs,
+	// which messes the test
+	os.Args = []string{""}
 	RunMain()
 	t.Log("Examplecobra ran successfully")
 }
